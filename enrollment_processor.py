59c080cb5bc44ff2e7c2dbaddd1214f8f3f19b7e
Candace Sheremeta <candace.aleea@gmail.com>
2016-07-09 10:03:01 -0400

    Add files via upload
    enrollment_processor.py uses the Selenium Webdriver for Python, instead of the python-mechanize module. So far, this script only opens up Firefox and googles for "Selenium". I had to change a few settings to get it to work with Firefox (the Python Selenium Webdriver only works with Firefox 45), but it's easy to take those lines out and use Chrome, instead.

---
+++ enrollment_processor.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+import time
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
+
+binary = FirefoxBinary('/opt/firefox-43/firefox')
+
+def init_driver():
+    driver = webdriver.Firefox(firefox_binary=binary)
+    driver.wait = WebDriverWait(driver, 5)
+    return driver
+
+def lookup(driver, query):
+    driver.get("http://www.google.com")
+    try:
+        box = driver.wait.until(EC.presence_of_element_located(
+            (By.NAME, "q")))
+        button = driver.wait.until(EC.element_to_be_clickable(
+            (By.NAME, "btnK")))
+        box.send_keys(query)
+        button.click()
+    except TimeoutException:
+        print("Box or Button not found in google.com")
+ 
+ 
+if __name__ == "__main__":
+    driver = init_driver()
+    lookup(driver, "Selenium")
+    time.sleep(5)
+    driver.quit()
+
